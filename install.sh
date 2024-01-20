04c31b3a56f4db4ec10f6b78a54e2c1f90dd9335
Mads Adrian Simonsen <47949834+simadma@users.noreply.github.com>
2024-01-20 18:44:16 +0000

    Add setup script that sets up git signing

---
+++ install.sh
@@ -0,0 +1,16 @@
+# Write SSH key to file
+mkdir -p ~/.ssh
+
+echo "$SSH_PRIVATE_KEY" > ~/.ssh/id_ed25519
+chmod 600 ~/.ssh/id_ed25519
+
+echo "$SSH_PUBLIC_KEY" > ~/.ssh/id_ed25519.pub
+chmod 644 ~/.ssh/id_ed25519.pub
+
+# Tell Git about the SSH key
+git config --global gpg.format ssh
+git config --global user.signingkey ~/.ssh/id_ed25519.pub
+
+# Sign commits by default
+git config --global commit.gpgsign true
+
