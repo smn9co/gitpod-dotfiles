d63bf98450eab2408ef5ea4003c8d3410d0f03f0
Mads Adrian Simonsen <47949834+simadma@users.noreply.github.com>
2024-01-21 08:58:07 +0000

    Add tree and subst echo to comment

--- install.sh
+++ install.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash
 
-echo "Starting installation..."
+echo -e "Starting installation...\n"
 
-echo -e "\nInstalling tmux..."
-sudo apt-get update && sudo apt-get install -y tmux
+sudo apt-get update && sudo apt-get install -y \
+  tmux \
+  tree
 
 # Changes the current directory to the directory of the script
 cd "$(dirname "$0")"
 
-echo -e "\nConfiguring git signing..."
+# Configuring git signing
 source scripts/git_sign_config.sh
 
-echo -e "\nInstalling Neovim..."
+# Installing Neovim
 source scripts/install_neovim.sh
 
 # Add Neovim config
