30703bda1c5a48274a36271432778e60a56ed91d
Mads Adrian Simonsen <47949834+simadma@users.noreply.github.com>
2024-03-08 07:43:09 +0000

    Rely on autoinstall instead on treesitter

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,27 +1,27 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      local configs = require("nvim-treesitter.configs")
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local configs = require("nvim-treesitter.configs")
 
-      configs.setup({
-        ensure_installed = {
-          "bash",
-          "html",
-          "javascript",
-          "json",
-          "lua",
-          "markdown",
-          "python",
-          "vim",
-          "vimdoc",
-        },
-        auto_install = true,
-        sync_install = false,
-        highlight = { enable = true },
-        indent = { enable = true },
-      })
-    end,
-  },
+			configs.setup({
+				ensure_installed = {
+					--  "bash",
+					--  "html",
+					--  "javascript",
+					--  "json",
+					--  "lua",
+					--  "markdown",
+					--  "python",
+					--  "vim",
+					--  "vimdoc",
+				},
+				auto_install = true,
+				sync_install = false,
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end,
+	},
 }
