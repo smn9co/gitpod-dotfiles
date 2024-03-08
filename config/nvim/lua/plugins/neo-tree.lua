5e7c207f7605337c679fc9dd1fe70c6ac38d2113
Mads Adrian Simonsen <47949834+simadma@users.noreply.github.com>
2024-03-08 06:19:12 +0000

    Ctrl + N now toggles the Neo-tree

--- config/nvim/lua/plugins/neo-tree.lua
+++ config/nvim/lua/plugins/neo-tree.lua
@@ -9,7 +9,7 @@ return {
       -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
     },
     config = function()
-      vim.keymap.set("n", "<C-n>", "<Cmd>Neotree<CR>")
+      vim.keymap.set("n", "<C-n>", "<Cmd>Neotree toggle<CR>")
     end
   }
 }
