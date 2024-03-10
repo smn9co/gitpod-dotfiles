545b392c4ee0219bfab914b009a1d3b585f7beb6
Mads Adrian Simonsen <47949834+simadma@users.noreply.github.com>
2024-03-10 10:15:59 +0000

    Add necessery parsers

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -6,17 +6,8 @@ return {
 			local configs = require("nvim-treesitter.configs")
 
 			configs.setup({
-				ensure_installed = {
-					--  "bash",
-					--  "html",
-					--  "javascript",
-					--  "json",
-					--  "lua",
-					--  "markdown",
-					--  "python",
-					--  "vim",
-					--  "vimdoc",
-				},
+        -- A list of parser names, or "all" (the five listed parsers should always be installed)
+				ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
 				auto_install = true,
 				sync_install = false,
 				highlight = { enable = true },
